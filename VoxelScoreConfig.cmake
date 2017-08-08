351aea76c9e2e82e15adba88625a3413119d83b8
Paul Hilbert <me@sicgraph.org>
2017-08-08 10:57:42 +0200

    added cmake configuration installation

---
+++ VoxelScoreConfig.cmake
@@ -0,0 +1,21 @@
+###############################################################################
+# Find VoxelScore
+#
+# This sets the following variables:
+# VOXEL_SCORE_FOUND - True if VOXEL_SCORE was found.
+# VOXEL_SCORE_INCLUDE_DIRS - Directories containing the VOXEL_SCORE include files.
+# VOXEL_SCORE_LIBRARY_DIRS - Directories containing the VOXEL_SCORE library.
+# VOXEL_SCORE_LIBRARIES - VOXEL_SCORE library files.
+
+if(WIN32)
+    find_path(VOXEL_SCORE_INCLUDE_DIR voxel_score PATHS "/usr/include" "/usr/local/include" "/usr/x86_64-w64-mingw32/include" "$ENV{PROGRAMFILES}" NO_DEFAULT_PATHS)
+else(WIN32)
+    find_path(VOXEL_SCORE_INCLUDE_DIR voxel_score PATHS "/usr/include" "/usr/local/include" "$ENV{PROGRAMFILES}" NO_DEFAULT_PATHS)
+endif(WIN32)
+
+set(VOXEL_SCORE_INCLUDE_DIRS ${VOXEL_SCORE_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(VOXEL_SCORE DEFAULT_MSG VOXEL_SCORE_INCLUDE_DIR)
+
+mark_as_advanced(VOXEL_SCORE_INCLUDE_DIR)
